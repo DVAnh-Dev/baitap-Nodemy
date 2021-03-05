6d3617041db6bb908e7b9ad8933078e02b08c4c9
Dao Anh <daoanh9617@gmail.com>
2021-03-05 17:21:37 +0700

    Sao lưu bài tập 5/3/21t

---
+++ baiTapNodemy/NodeJS/index1.js
@@ -0,0 +1,17 @@
+// var a = 10;
+// var b = 5;
+// var c = a + b;
+// var d = a - b;
+// var e = a / b;
+// var f = a * b;
+// console.log(e);
+// var today = 'sunday';
+// var a = 10;
+// var DVanh = true;
+// console.log(today, a, DVanh);
+var DVAnh1 = {
+    gender: 'male',
+    age: 22,
+    name: 'DVAnh'
+}
+console.log(DVAnh1);
